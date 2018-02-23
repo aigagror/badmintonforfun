b67e753695c3f285ac79fcab7c8fd07c67f60b12
Bhuvan Venkatesh <bhuvan.venkatesh21@gmail.com>
2018-02-23 14:58:44 -0600

    Started working on column system

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,6 +3,8 @@ import * as ReactDOM from "react-dom";
 
 import { Hello } from "./components/Hello";
 
+import './sass/grid.scss';
+
 ReactDOM.render(
     <Hello compiler="TypeScript" framework="React" />,
     document.getElementById("example")
--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,5 +1,7 @@
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
 module.exports = {
-    entry: "./src/index.tsx",
+    entry: ["./src/index.tsx"],
     output: {
         filename: "bundle.js",
         path: __dirname + "/dist"
@@ -10,23 +12,38 @@ module.exports = {
 
     resolve: {
         // Add '.ts' and '.tsx' as resolvable extensions.
-        extensions: [".ts", ".tsx", ".js", ".json"]
+        extensions: [".ts", ".tsx", ".js", ".json", ".scss", ".sass", ".css"]
     },
 
     module: {
         rules: [
-            // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
-            { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
 
-            // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
-            { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
+            {
+                test: /\.tsx?$/,
+                loader: "awesome-typescript-loader"
+            },
+            {
+                enforce: "pre",
+                test: /\.js$/,
+                loader: "source-map-loader" 
+            },
+            {
+              test: /\.scss$/,
+              use: ExtractTextPlugin.extract({
+                fallback: 'style-loader',
+                use: ['css-loader', 'sass-loader']
+              })
+            }
         ]
     },
 
-    // When importing a module whose path matches one of the following, just
-    // assume a corresponding global variable exists and use that instead.
-    // This is important because it allows us to avoid bundling all of our
-    // dependencies, which allows browsers to cache those libraries between builds.
+    plugins: [
+        new ExtractTextPlugin({ // define where to save the file
+            filename: '[name].css',
+            allChunks: true,
+        }),
+    ],
+
     externals: {
         "react": "React",
         "react-dom": "ReactDOM"
