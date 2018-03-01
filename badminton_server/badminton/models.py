baee40c80b77e527feeb86d90251bbf222c943f5
Eddie Huang <aigagror@gmail.com>
2018-02-28 18:47:08 -0600

    Added more of the schema

--- badminton_server/badminton/models.py
+++ badminton_server/badminton/models.py
@@ -6,6 +6,18 @@ from django.utils import timezone
 
 # Create your models here.
 
+JOBS = (
+        ('PRESIDENT', 'President'),
+        ('TREASURER', 'Treasurer'),
+        ('OFFICER', 'Officer'),
+    )
+
+QUEUE_TYPE = (
+        ('CASUAL', 'Casual'),
+        ('RANKED', 'Ranked'),
+        ('KOTH', 'King of the Hill'),
+    )
+
 class Interested(models.Model):
     name = models.CharField(max_length=64)
     formerBoardMember = models.BooleanField(default=False)
@@ -22,10 +34,35 @@ class Member(Interested):
 
 
 class BoardMember(Member):
-    JOBS = (
-        ('PRESIDENT', 'President'),
-        ('TREASURER', 'Treasurer'),
-        ('OFFICER', 'Officer'),
-    )
     job = models.CharField(max_length=64, choices=JOBS)
 
+class Election(models.Model):
+    date = models.DateField('date of the election')
+
+class Votes(models.Model):
+    votee = models.ForeignKey(Member, on_delete=models.SET_NULL)
+    election = models.ForeignKey(Election, on_delete=models.CASCADE)
+    voter = models.ForeignKey(Member, on_delete=models.CASCADE)
+
+class Campaign(models.Model):
+    job = models.CharField(max_length=64, choices=JOBS)
+    pitch = models.CharField(max_length=500)
+    election = models.ForeignKey(Election, on_delete=models.CASCADE)
+    campaigner = models.ForeignKey(Member, on_delete=models.CASCADE)
+
+class Queue(models.Model):
+    type = models.CharField(max_length=64, choices=QUEUE_TYPE)
+
+class Team(models.Model):
+    memberA = models.ForeignKey(Member, on_delete=models.CASCADE)
+    memberB = models.ForeignKey(Member, on_delete=models.SET_NULL)
+
+class Match(models.Model):
+    startDate = models.DateTimeField('date started')
+    scoreA = models.IntegerField()
+    scoreB = models.IntegerField()
+    teamA = models.ForeignKey(Team, on_delete=models.SET_NULL)
+    teamB = models.ForeignKey(Team, on_delete=models.SET_NULL)
+
+class FinishedMatch(Match):
+    endDate = models.DateTimeField('date ended')
