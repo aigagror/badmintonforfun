07aaf93317b705799cb55377a786e859133fe96c
Eddie Huang <aigagror@gmail.com>
2018-02-28 21:45:02 -0600

    Moved models from badminton into api

--- badminton_server/badminton/admin.py
+++ badminton_server/badminton/admin.py
@@ -1,7 +1,4 @@
 from django.contrib import admin
 
 from .models import *
-# Register your models here.
-admin.site.register(Interested)
-admin.site.register(Member)
-admin.site.register(BoardMember)
+# Register your models here.)
