62e92a51cd9b1e30a89e5ac98d69789c437e3dd2
Eddie Huang <aigagror@gmail.com>
2018-02-28 18:25:38 -0600

    Started adding some models

--- badminton_server/badminton/models.py
+++ badminton_server/badminton/models.py
@@ -1,3 +1,31 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
+
 
 # Create your models here.
+
+class Interested(models.Model):
+    name = models.CharField(max_length=64)
+    formerBoardMember = models.BooleanField(default=False)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.name
+
+
+class Member(Interested):
+    level = models.IntegerField(min_value=0, default=0)
+    private = models.BooleanField(default=False)
+    dateJoined = models.DateField('date joined')
+
+
+class BoardMember(Member):
+    JOBS = (
+        ('PRESIDENT', 'President'),
+        ('TREASURER', 'Treasurer'),
+        ('OFFICER', 'Officer'),
+    )
+    job = models.CharField(choices=JOBS)
+
