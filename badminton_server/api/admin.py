161f2d570b5012e53de832923eeb42999a4891c4
Eddie Huang <aigagror@gmail.com>
2018-03-03 13:51:07 -0600

    Translated everything to raw_sql

--- badminton_server/api/admin.py
+++ badminton_server/api/admin.py
@@ -2,9 +2,6 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
-admin.site.register(Interested)
-admin.site.register(Member)
-admin.site.register(BoardMember)
-admin.site.register(Queue)
 admin.site.register(Court)
-admin.site.register(Tournament)
+admin.site.register(Election)
+admin.site.register(Interested)
