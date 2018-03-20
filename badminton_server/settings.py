f8830c707edbb379046c840cdf9112152a6169f1
Eddie Huang <aigagror@gmail.com>
2018-03-20 01:51:28 +0000

    Stuff from pythonanywhere.com

--- badminton_server/settings.py
+++ badminton_server/settings.py
@@ -118,8 +118,14 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.0/howto/static-files/
+# https://docs.djangoproject.com/en/dev/howto/static-files/
 
+STATIC_URL = '/static/'
+
+# default static files settings for PythonAnywhere.
+# see https://help.pythonanywhere.com/pages/DjangoStaticFiles for more info
+MEDIA_ROOT = u'/home/aigagror/badmintonforfun/media'
+MEDIA_URL = '/media/'
+STATIC_ROOT = u'/home/aigagror/badmintonforfun/static'
 STATIC_URL = '/static/'
