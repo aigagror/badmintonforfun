7aedbb48d6f564367a17b3f2d4cb523d5112f587
Daniel Rong <drong214@gmail.com>
2018-03-20 19:13:39 -0500

    Finished Settings page API

--- api/settings_api.py
+++ api/settings_api.py
@@ -1,48 +1,38 @@
-from django.db import connection
+from django.db import connection, IntegrityError
 from .cursor import *
-
+import json
 
 # Members
-def edit_privacy(email, new_privacy):
+def edit_member_info(email, attribute, new_value):
+    """
+    Edits the specified attribute of a member.
+    The attributes available to edit are 'level', 'private', 'dateJoined', 'bio', and possibly 'queue'?
+    :param email:
+    :param attribute:
+    :param new_value:
+    :return:
+    """
     with connection.cursor() as cursor:
         query = '''
         UPDATE api_member
-        SET privacy = '%s'
+        SET ''' + attribute + '''=%s
         WHERE interested_ptr_id=%s;
         '''
-        cursor.execute(query, [new_privacy, email])
-        results = dictfetchall(cursor)
-    return results
+        cursor.execute(query, [new_value, email])
 
 
-def get_privacy(email):
+def get_member_attr(email, attribute):
+    """
+    Retrieves the specified attribute of a member.
+    The attributes available to retrieve are 'level', 'private', 'dateJoined', 'bio', and possibly 'queue'?.
+    Will be used to show the member their current settings.
+    :param email:
+    :param attribute:
+    :return:
+    """
     with connection.cursor() as cursor:
         query = '''
-        SELECT private
-        FROM api_member
-        WHERE interested_ptr_id=%s;
-        '''
-        cursor.execute(query, [email])
-        results = dictfetchall(cursor)
-    return results
-
-
-def edit_bio(email, new_bio):
-    with connection.cursor() as cursor:
-        query = '''
-        UPDATE api_member
-        SET bio = '%s'
-        WHERE interested_ptr_id=%s;
-        '''
-        cursor.execute(query, [new_bio, email])
-        results = dictfetchall(cursor)
-    return results
-
-
-def get_bio(email):
-    with connection.cursor() as cursor:
-        query = '''
-        SELECT bio
+        SELECT ''' + attribute + '''
         FROM api_member
         WHERE interested_ptr_id=%s;
         '''
@@ -59,7 +49,7 @@ def get_interested():
     """
     with connection.cursor() as cursor:
         query = '''
-        SELECT first_name, last_name, email
+        SELECT *
         FROM api_interested
         WHERE email NOT IN (
             SELECT interested_ptr_id
@@ -78,7 +68,7 @@ def get_members():
     """
     with connection.cursor() as cursor:
         query = '''
-        SELECT first_name, last_name, email
+        SELECT *
         FROM api_interested, api_member
         WHERE api_interested.email = interested_ptr_id;
         '''
@@ -94,7 +84,7 @@ def get_board_members():
     """
     with connection.cursor() as cursor:
         query = '''
-        SELECT first_name, last_name, email, job
+        SELECT *
         FROM api_interested, api_boardmember
         WHERE email = member_ptr_id;
         '''
@@ -103,7 +93,7 @@ def get_board_members():
     return results
 
 
-def delete(email):
+def remove_member(email):
     """
     Deletes the tuple in api_interested with 'email'. This should delete any related tuples in
     api_member and api_boardmember.
@@ -113,12 +103,22 @@ def delete(email):
     """
     with connection.cursor() as cursor:
         query = '''
-        DELETE FROM api_interested
-        WHERE email=%s;
-        '''
+                DELETE FROM api_boardmember
+                WHERE member_ptr_id=%s;
+                '''
         cursor.execute(query, [email])
-        results = dictfetchall(cursor)
-    return results
+
+        query = '''
+                DELETE FROM api_member
+                WHERE interested_ptr_id=%s;
+                '''
+        cursor.execute(query, [email])
+
+        query = '''
+                DELETE FROM api_interested
+                WHERE email=%s;
+                '''
+        cursor.execute(query, [email])
 
 
 def add_interested(interested):
@@ -132,9 +132,12 @@ def add_interested(interested):
         INSERT INTO api_interested (first_name, last_name, formerBoardMember, email)
         VALUES (%s, %s, %s, %s);
         '''
-        cursor.execute(query, [interested.first_name, interested.last_name, interested.formerBoardMember, interested.email])
-        results = dictfetchall(cursor)
-    return results
+        try:
+            cursor.execute(query, [interested.first_name, interested.last_name, interested.formerBoardMember, interested.email])
+        except IntegrityError:
+            return json.dumps({'code': 400, 'message': 'This person is already in the club.'})
+        else:
+            return json.dumps({'code': 200, 'message': 'OK'})
 
 
 def promote_to_member(email, member):
@@ -146,12 +149,16 @@ def promote_to_member(email, member):
     """
     with connection.cursor() as cursor:
         query = '''
-        INSERT INTO api_member (interested_ptr_id, level, private, dateJoined, queue, bio)
-        VALUES (%s, %s, %s, %s. %s, %s);
+        INSERT INTO api_member (interested_ptr_id, level, private, dateJoined, bio)
+        VALUES (%s, %s, %s, %s, %s);
         '''
-        cursor.execute(query, [email, member.level, member.private, member.dateJoined, member.queue, member.bio])
-        results = dictfetchall(cursor)
-    return results
+        try:
+            arr = [email, member.level, member.private, member.dateJoined, member.bio]
+            cursor.execute(query, arr)
+        except IntegrityError:
+            return json.dumps({'code': 400, 'message': 'This person is already a member.'})
+        else:
+            return json.dumps({'code': 200, 'message': 'OK'})
 
 
 def promote_to_board_member(email, board_member):
@@ -166,34 +173,169 @@ def promote_to_board_member(email, board_member):
         INSERT INTO api_boardmember (member_ptr_id, job)
         VALUES (%s, %s);
         '''
-        cursor.execute(query, [email, board_member.job])
+        try:
+            cursor.execute(query, [email, board_member.job])
+        except IntegrityError:
+            return json.dumps({'code': 400, 'message': 'This person is already a board member.'})
+        else:
+            return json.dumps({'code': 200, 'message': 'OK'})
+
+
+def add_to_schedule(date, number_of_courts):
+    """
+    Allow board members to insert the number of available courts on the specified date.
+    :param date:
+    :param number_of_courts: If this is 4, only courts 1-4 will be available
+    :return:
+    """
+    with connection.cursor() as cursor:
+        query = '''
+                INSERT INTO api_schedule (date, number_of_courts)
+                VALUES (%s, %s)
+                '''
+        cursor.execute(query, [date, number_of_courts])
+
+
+def edit_schedule(date, number_of_courts):
+    """
+    Edit existing entry in api_schedule.
+    :param date:
+    :param number_of_courts:
+    :return:
+    """
+    with connection.cursor() as cursor:
+        query = '''
+                UPDATE api_schedule
+                SET number_of_courts=%s
+                WHERE date=%s
+                '''
+        cursor.execute(query, [number_of_courts, date])
+
+def delete_from_schedule(date):
+    """
+    Delete existing entry in api_schedule.
+    :param date:
+    :return:
+    """
+    with connection.cursor() as cursor:
+        query = '''
+                DELETE FROM api_schedule
+                WHERE date=%s;
+                '''
+        cursor.execute(query, [date])
+
+
+def get_schedule():
+    """
+    Returns all the tuples in api_schedule in descending order
+    :return:
+    """
+    with connection.cursor() as cursor:
+        query = '''
+        SELECT * 
+        FROM api_schedule
+        ORDER BY date DESC;
+        '''
+        cursor.execute(query)
         results = dictfetchall(cursor)
     return results
 
 
-def delete_member(email):
-    foo = 0
+def get_available_courts(date):
+    """
+    Returns the courts available on 'date'
+    :param date:
+    :return:
+    """
+    with connection.cursor() as cursor:
+        query = '''
+        SELECT *
+        FROM api_court
+        WHERE id <=
+            (SELECT number_of_courts
+            FROM api_schedule
+            WHERE date=%s);
+        '''
+        cursor.execute(query, [date])
+        results = dictfetchall(cursor)
+    return results
 
 
-def edit_member(email, attribute, new_value):
-    foo = 0
+def get_all_courts():
+    """
+    Returns all courts
+    :return:
+    """
+    with connection.cursor() as cursor:
+        query = '''
+            SELECT *
+            FROM api_court;
+            '''
+        cursor.execute(query)
+        results = dictfetchall(cursor)
+    return results
 
 
-def add_court():
-    foo = 0
+def get_court(court_id):
+    with connection.cursor() as cursor:
+        query = '''
+        SELECT *
+        FROM api_court
+        WHERE id=%s;
+        '''
+        cursor.execute(query, [court_id])
+        results = dictfetchall(cursor)
+    return results
 
 
-def remove_court():
-    foo = 0
+def add_court(court):
+    with connection.cursor() as cursor:
+        query = '''
+        INSERT INTO api_court (id, number, queue_id)
+        VALUES (%s, %s, %s)
+        '''
+        try:
+            cursor.execute(query, [court.id, court.number, court.queue])
+        except IntegrityError:
+            return json.dumps({'code': 400, 'message': 'This court already exists.'})
+        else:
+            return json.dumps({'code': 200, 'message': 'OK'})
 
 
-def add_queue():
-    foo = 0
+def edit_court_info(court_id, attribute, new_value):
+    """
+    Used to edit any attribute of the court with id=court_id. The attributes
+    available to edit are 'id', 'number', and 'queue_id'.
+    :param court_id: 
+    :param attribute: 
+    :param new_value: 
+    :return: 
+    """
+    with connection.cursor() as cursor:
+        query = '''
+        UPDATE api_court
+        SET ''' + attribute + '''=%s
+        WHERE id=%s;
+        '''
+        try:
+            cursor.execute(query, [new_value, court_id])
+        except IntegrityError:
+            return json.dumps({'code': 400, 'message': 'This queue type does not exist.'})
+        else:
+            return json.dumps({'code': 200, 'message': 'OK'})
 
 
-def remove_queue():
-    foo = 0
+def add_queue(queue):
+    with connection.cursor() as cursor:
+        query = '''
+        INSERT INTO api_queue (type)
+        VALUES (%s)
+        '''
+        try:
+            cursor.execute(query, [queue.type])
+        except IntegrityError:
+            return json.dumps({'code': 400, 'message': 'This queue type already exists.'})
+        else:
+            return json.dumps({'code': 200, 'message': 'OK'})
 
 
-def edit_queue():
-    foo = 0
--- api/views.py
+++ api/views.py
@@ -80,15 +80,15 @@ class Interested(object):
 class Member(object):
     level = ''
     private = False
-    date_joined = ''
-    queue = None
+    dateJoined = ''
+    # queue = ''
     bio = ''
 
-    def __init__(self, level, private, date_joined, queue, bio):
+    def __init__(self, level, private, dateJoined, bio):
         self.level = level
         self.private = private
-        self.date_joined = date_joined
-        self.queue = queue
+        self.dateJoined = dateJoined
+        # self.queue = queue
         self.bio = bio
 
 
@@ -99,14 +99,73 @@ class BoardMember(object):
         self.job = job
 
 
+class Court(object):
+    id = ''
+    number = ''
+    queue = ''
+
+    def __init__(self, id, number, queue):
+        self.id = id
+        self.number = number
+        self.queue = queue
+
+
+class Queue(object):
+    type = ''
+
+    def __init__(self, type):
+        self.type = type
+
 def settings(request):
+    #remove_member('johndoe@email.com')
+    #add_interested(Interested('John', 'Doe', False, 'johndoe@email.com'))
+    #new_member_info = Member(0, False, '2018-03-20', 'My name is John Doe and I like badminton')
+    #promote_to_member('johndoe@email.com', new_member_info)
+    #promote_to_board_member('johndoe@email.com', BoardMember('Test Job'))
+
+    #edit_member_info('johndoe@email.com', 'private', True)
+    #edit_bio('johndoe@email.com', 'edit 2')
+    #edit_privacy('johndoe@email.com', False)
+
+    #remove_member('johndoe@email.com')
+
     board_members = get_board_members()
     members = get_members()
     interested = get_interested()
+    john_doe_attr = get_member_attr('johndoe@email.com', 'private')
+
+    #add_to_schedule('2018-03-21', 8)
+    #edit_schedule('2018-03-21', 2)
+    #delete_from_schedule('2018-03-21')
+    schedule = get_schedule()
+
+    #add_queue(Queue('CASUAL'))
+    #add_queue(Queue('RANKED'))
+    #add_queue(Queue('KOTH'))
+
+    # add_court(Court(1, 4, 'CASUAL'))
+    # add_court(Court(2, 2, 'RANKED'))
+    # add_court(Court(3, 8, 'CASUAL'))
+    # add_court(Court(4, 4, 'KOTH'))
+    # add_court(Court(5, 2, 'RANKED'))
+    # add_court(Court(6, 2, 'RANKED'))
+    # add_court(Court(7, 2, 'RANKED'))
+    # add_court(Court(8, 2, 'RANKED'))
+
+    check_court_date = '2018-03-31'
+    available_courts = {'date': check_court_date, 'courts': get_available_courts(check_court_date)}
+
+    edit_court_info(1, 'queue_id', 'KOTH')
+    all_courts = get_all_courts()
+
     context = {
         'board_members': board_members,
         'members': members,
-        'interested': interested
+        'interested': interested,
+        'john_doe_attr': john_doe_attr,
+        'schedule': schedule,
+        'available_courts': available_courts,
+        'all_courts': all_courts
     }
 
     return render(request, 'api_settings.html', context)
