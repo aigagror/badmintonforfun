20cc438341035d96b807151420bcc76f131c1bba
Daniel Rong <drong214@gmail.com>
2018-03-24 18:33:41 -0500

    added advanced query 1 to file

--- advanced_queries.sql
+++ advanced_queries.sql
@@ -1,8 +1,17 @@
 -- TODO: Setup some mock data to demonstrate the queries
 
 
-SELECT "First Advanced Query: Party on the queue with the minimum average play time of the members involved from today";
-
+SELECT "First Advanced Query: Party on the queue (%s) with the minimum average play time of the members involved from today";
+SELECT party.id AS party_id, party.leader_id, AVG (play_time) AS average_party_play_time_seconds
+FROM api_party AS party, api_member AS m1 JOIN
+(
+    SELECT pin.member_id, SUM((julianday(fm.endDate)-julianday(m2.startDate))*86400.0) AS play_time
+    FROM api_match AS m2 JOIN api_finishedmatch fm JOIN api_playedin AS pin
+    WHERE m2.id=fm.match_ptr_id AND m2.id=pin.match_id AND date(m2.startDate) > date(julianday())
+    GROUP BY pin.member_id
+)
+WHERE party.id=m1.party_id AND m1.interested_ptr_id=member_id AND party.queue_id = %s
+GROUP BY party.id ORDER BY play_time ASC LIMIT 1;
 
 
 
