0babb701dd5e625b7e6291cc66e9e8b9cfd155e6
Eddie Huang <aigagror@gmail.com>
2018-03-24 17:46:33 -0500

    Used the keywords INNER JOIN

--- advanced_queries.sql
+++ advanced_queries.sql
@@ -9,7 +9,8 @@ SELECT "First Advanced Query: Party on the queue with the minimum average play t
 SELECT "Second Advanced Query: Members in order of highest wins/total games ratio";
 SELECT member.interested_ptr_id, COUNT(CASE WHEN (playedin.team = 'A' AND match.scoreA > match.scoreB) OR
                                                  (playedin.team = 'B' AND match.scoreB > match.scoreA) THEN 1 ELSE NULL END) AS wins, COUNT(*) AS total_games
-FROM api_member AS member, api_match AS match, api_playedin AS playedin
-WHERE member.interested_ptr_id = playedin.member_id AND match.id = playedin.match_id
+FROM (api_member AS member
+  INNER JOIN api_playedin AS playedin ON member.interested_ptr_id = playedin.member_id)
+  INNER JOIN api_match AS match ON match.id = playedin.match_id
 GROUP BY member.interested_ptr_id
 ORDER BY wins * 1.0 / total_games DESC;
