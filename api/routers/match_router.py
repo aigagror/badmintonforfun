6eb42261f8f7a44e53e2028d08282cf68efa4626
grshen80 <grshen80@gmail.com>
2018-04-03 18:59:25 -0500

    added finish match and edit match so running the server should work now

--- api/routers/match_router.py
+++ api/routers/match_router.py
@@ -1,5 +1,6 @@
 from api.calls.match_call import get_top_players
 from api.routers.router import restrictRouter
+from .router import validate_keys
 
 
 @restrictRouter(allowed=["GET"])
@@ -11,3 +12,20 @@ def top_players(request):
     :return:
     """
     return get_top_players()
+
+@restrictRouter(allowed=["POST"])
+def edit_match(request):
+    """
+    POST -- edits the match score, provide the score of the match
+        Required Keys: id (match ID), score_A, score_B
+    :param request:
+    :return:
+    """
+
+    dict_post = dict(request.POST.items())
+    validate_keys(["score_A", "score_B", "id"],dict_post)
+    return edit_match(dict_post["id"], dict_post["score_A"], dict_post["score_B"])
+
+@restrictRouter(allowed=["POST"])
+def finish_match(request):
+    return None
