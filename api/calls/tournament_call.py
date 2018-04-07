d80f62c1faec7df47ceeae58239a977cc2968392
Eddie Huang <aigagror@gmail.com>
2018-04-07 14:32:11 -0500

    Get tournament

--- api/calls/tournament_call.py
+++ api/calls/tournament_call.py
@@ -1,5 +1,6 @@
 from api.cursor_api import http_response
 from api.models import *
+from django.db import connection
 
 def get_most_recent_tournament():
     """
@@ -9,7 +10,7 @@ def get_most_recent_tournament():
     most_recent_tournaments = Tournament.objects.raw("SELECT * FROM api_tournament ORDER BY date LIMIT 1")
     if len(list(most_recent_tournaments)) == 0:
         # No tournaments
-        return http_response(message="No tournaments exists")
+        return http_response(message="No tournaments exist", code=400)
 
     most_recent_tournament = most_recent_tournaments[0]
     t_id = most_recent_tournament.id
@@ -19,20 +20,26 @@ def get_most_recent_tournament():
     tournament_bracket_nodes = list(tournament_bracket_nodes)
     # Get max 'level' among those bracket nodes (this will determine the depth of our bracket node dictionary
     # the 'root' tournament bracket node will be at level 0
-    rawquery = BracketNode.objects.raw("SELECT MAX(level) AS max_level FROM api_bracketnode")
-    max_level = rawquery[0].max_level
+    with connection.cursor() as cursor:
+        cursor.execute("SELECT MAX(level) AS max_level FROM api_bracketnode")
+        row = cursor.fetchone()
+
+        max_level = row[0]
 
     bracket_nodes = _build_bracket_dictionary(tournament_bracket_nodes, max_level, 0, 0)
 
     tournament_dict = {
         "tournament": {
             "tournament_id": most_recent_tournament.id,
-            "date": most_recent_tournament.date,
-            "end_date": most_recent_tournament.endDate,
+            "date": serializeDate(most_recent_tournament.date),
+            "end_date": serializeDate(most_recent_tournament.endDate)if most_recent_tournament.endDate is not None else "None",
             "bracket_nodes": bracket_nodes
         }
     }
-    return tournament_dict
+
+
+
+    return http_response(tournament_dict)
 
 
 def _build_bracket_dictionary(bracket_nodes, max_level, curr_level, curr_sibling_index):
