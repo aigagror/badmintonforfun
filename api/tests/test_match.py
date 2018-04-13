bf7fb86339747027c7d1ea9f506fdb4954a39498
Eddie Huang <aigagror@gmail.com>
2018-04-13 15:43:36 -0500

    Removed the id field from finish_match tests. Added more test cases to the rankings test

--- api/tests/test_match.py
+++ api/tests/test_match.py
@@ -34,7 +34,7 @@ class MatchTest(CustomTestCase):
         self.assertTrue('teamA' in match)
         self.assertTrue('teamB' in match)
 
-    @run(path_name='finish_match', email=GRACE, method=POST, args={'id': 9, 'scoreA': 21, 'scoreB': 19})
+    @run(path_name='finish_match', email=GRACE, method=POST, args={'scoreA': 21, 'scoreB': 19})
     def test_finish_match(self):
         """
         Grace is associated with one unfinished match,
--- api/tests/test_rankings.py
+++ api/tests/test_rankings.py
@@ -2,8 +2,17 @@ from .custom_test_case import *
 
 class RankingTest(CustomTestCase):
 
-    @run(path_name='ranking', email=MEMBER, method=GET, args={})
-    def test_get_ranking(self):
+    def _assert_fields(self, topPlayer):
+        self.assertTrue('member_info' in topPlayer)
+        member_info = topPlayer['member_info']
+        self.assertTrue('first_name' in member_info)
+
+        self.assertTrue('number_of_total_games' in topPlayer)
+        self.assertTrue('number_of_won_games' in topPlayer)
+        self.assertTrue('level' in topPlayer)
+
+    @run(path_name='get_rankings_by_level', email=MEMBER, method=GET, args={})
+    def test_get_rankings_by_level(self):
         response = self.response
         self.assertGoodResponse(response)
 
@@ -17,8 +26,31 @@ class RankingTest(CustomTestCase):
 
         topPlayer = rankings[0]
 
-        self.assertTrue('member' in topPlayer)
+        self._assert_fields(topPlayer)
 
-        self.assertTrue('number_of_total_games' in topPlayer)
+        # Joshua should be the top player, since only he has a level greater than 0 (10).
+        self.assertEqual(topPlayer['member_info']['first_name'], 'Joshua')
 
-        self.assertTrue('number_of_won_games' in topPlayer)
+
+
+
+    @run(path_name='get_rankings_by_win_ratio', email=MEMBER, method=GET, args={})
+    def test_get_rankings_by_win_ratio(self):
+        response = self.response
+        self.assertGoodResponse(response)
+
+        json = response.json()
+
+        self.assertTrue('rankings' in json)
+
+        rankings = json['rankings']
+
+        self.assertTrue(isinstance(rankings, (list,)))
+
+        topPlayer = rankings[0]
+
+        self._assert_fields(topPlayer)
+
+        # Eddie should be the top player, since only he has a win ratio of 1
+        self.assertEqual(topPlayer['member_info']['first_name'], 'Eddie')
+
