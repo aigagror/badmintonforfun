c826f77568ed36fd1ed753e15e8eac4a265071c5
Eddie Huang <aigagror@gmail.com>
2018-04-14 11:07:12 -0500

    Added test case for courts

---
+++ api/tests/test_court.py
@@ -0,0 +1,45 @@
+import datetime
+
+from django.test import TestCase
+from django.urls import reverse
+
+from api import cursor_api
+from api.models import *
+from api.calls.match_call import *
+from .custom_test_case import *
+import json
+
+class CourtTest(CustomTestCase):
+
+    @run(path_name='get_courts', email=JARED, method=GET, args={})
+    def test_get_courts(self):
+        response = self.response
+        self.assertGoodResponse(response)
+
+        json = response.json()
+
+        self.assertTrue('courts' in json)
+
+        courts = json['courts']
+
+        self.assertGreater(len(courts), 0)
+
+
+        some_courts_have_matches = False
+        some_courts_are_empty = False
+        for court in courts:
+            self.assertTrue('queue' in court)
+            self.assertTrue('match' in court)
+            match = court['match']
+
+            if match is not None:
+                some_courts_have_matches = True
+                self.assertTrue('id' in match)
+                self.assertTrue('teamA' in match)
+                self.assertTrue('teamB' in match)
+            else:
+                some_courts_are_empty = True
+
+
+        self.assertTrue(some_courts_have_matches)
+        self.assertTrue(some_courts_are_empty)
