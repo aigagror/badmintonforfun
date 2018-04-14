ece08e76b10c88ff33e958534206b3062c018ff1
Eddie Huang <aigagror@gmail.com>
2018-04-14 14:56:39 -0500

    Code for preprocessing csv

--- api/fixtures/foo.py
+++ api/fixtures/foo.py
@@ -1,8 +1,11 @@
 import pandas
 
-x = pandas.read_csv('foo.csv')
-print(x)
 
-y = pandas.read_csv('bar.csv')
-print(y)
+data_files = ['foo.csv', 'bar.csv']
 
+for file in data_files:
+    foo = pandas.read_csv(file)
+    for row_index, bar in foo[['email', 'full_name']].iterrows():
+        first_name, last_name = bar.full_name.split()
+        email = bar.email
+        x = 0
