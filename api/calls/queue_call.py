427ba401d74a1f0da4c6d696f0bb59431c7ffd31
Daniel Rong <drong214@gmail.com>
2018-04-22 18:16:33 -0500

    Changed playtime to minutes instead of seconds

--- api/calls/queue_call.py
+++ api/calls/queue_call.py
@@ -1,30 +1,73 @@
+import datetime
+import json
+
 from api.cursor_api import *
-from api.models import Queue, Party, Member
-from api.cursor_api import http_response, dictfetchall
+from api.models import Queue, Party, Member, Court, Match
+from api.cursor_api import http_response, dictfetchall, run_connection, serializeDateTime
 from operator import itemgetter
+
+"""
+    FUNCTIONS: (*) = not sure if works, look at this later
+    get_queues() returns an http response
+    get_parties_by_playtime(queue_type) returns an http_response
+    delete_queue(id) returns an http response
+    get_queue_by_id(id) returns a boolean (T/F whether there's a queue with id)
+    get_queue_by_type(queue_type) returns a dictionary, or None
+    get_queue_type(queue_id) returns a string
+    edit_queue(id, type) returns an http response
+    create_queue(id) returns an http response
+"""
 
 
 def get_queues():
+    """
+    Returns a dictionary; "queues" has the queue ID as id, type, and parties in the queue;
+            parties have the party ID as id, queue ID as queue, a list of members, and the number of members in
+            the party
+    :return:
+    """
     dict = {}
     dict['queues'] = []
-    queues = Queue.objects.raw("SELECT * FROM api_queue")
+    queues = Queue.objects.raw("SELECT * FROM api_queue");
     if len(list(queues)) >= 1:
         for queue in queues:
-            queue_dict = serializeModel(queue)
-            queue_dict['parties'] = []
+            queue_dict = {
+                "type": queue.type,
+                "id": queue.id,
+                "parties": []
+            }
             parties = Party.objects.raw("SELECT * FROM api_party WHERE queue_id = %s", [queue.id])
+
+            response = get_parties_by_playtime(queue.type)
+            content = response.content.decode()
+            content = json.loads(content)
+
             for party in parties:
                 party_dict = serializeModel(party)
 
                 members = Member.objects.raw("SELECT * FROM api_member WHERE party_id = %s", [party.id])
-                members_dict = serializeSetOfModels(members)
+                members_dict = [{
+                    "first_name": member.first_name,
+                    "last_name": member.last_name,
+                    "id": member.id
+                } for member in members]
                 party_dict['members'] = members_dict
-                party_dict['number of members'] = len(members_dict)
+                party_dict['num_members'] = len(members_dict)
+
+                response_parties = content['parties']
+                curr_party_avg_play_time = 0
+                for i in range(len(response_parties)):
+                    if response_parties[i]['party_id'] == party.id:
+                        curr_party_avg_play_time = round(response_parties[i]['avg_time'], 3)  # in seconds
+                        break
+
+                party_dict['average_play_time'] = round(curr_party_avg_play_time/60, 3)
 
                 queue_dict['parties'].append(party_dict)
 
             dict['queues'].append(queue_dict)
 
+        print(dict)
         return http_response(dict)
     else:
         return http_response({}, message="There are no queues.", code=200)
@@ -58,6 +101,7 @@ def get_parties_by_playtime(queue_type):
                                     AND api_party.queue_id=api_queue.id) AND api_match.id=api_playedin.match_id
                                     GROUP BY api_playedin.member_id, api_match.id) AS party_members
                                     GROUP BY party_members.member_id
+                                    ORDER BY play_time ASC 
                                      '''
 
         cursor.execute(query_members_playtime, [queue_type])
@@ -80,7 +124,7 @@ def get_parties_by_playtime(queue_type):
 
         for i in members_with_playtime:
             for j in parties:
-                if i["member_id"] in j["member_ids"]:
+                if i["member_id"] in j["member_ids"] and i["member_play_time"] is not None:
                     j["avg_time"] += i["member_play_time"]
 
         for i in parties:
@@ -106,7 +150,6 @@ def get_queue_by_id(id):
     with connection.cursor() as cursor:
         cursor.execute("SELECT * FROM api_queue WHERE id=%s", [id])
         result = dictfetchone(cursor)
-        print(result)
         if result:
             return True
         else:
@@ -121,6 +164,15 @@ def get_queue_by_type(queue_type):
             return result
         else:
             return None
+
+
+def get_queue_type(queue_id):
+    rawquery = Queue.objects.raw("SELECT * FROM api_queue WHERE id=%s", [str(queue_id)])
+    if len(list(rawquery)) == 0:
+        return None
+    else:
+        queue = rawquery[0]
+        return str(queue.type)
 
 
 def edit_queue(id, type):
@@ -143,3 +195,88 @@ def create_queue(type):
     """
 
     return run_connection("INSERT INTO api_queue (type) VALUES (%s)", type)
+
+
+def dequeue_party_to_court_call(queue_type):
+    response = get_parties_by_playtime(queue_type)
+    my_json = json.loads(response.content.decode())
+    parties = my_json['parties']
+    if len(parties) == 0:
+        return http_response({}, message='No parties on this queue', code=400)
+    party_to_dequeue = parties[0]
+    party_id = party_to_dequeue['party_id']
+    queues = Queue.objects.raw("SELECT * FROM api_queue WHERE type = %s", [queue_type])
+    if len(list(queues)) == 0:
+        return http_response({}, message='No such queue found', code=400)
+    queue = queues[0]
+    queue_courts = Court.objects.raw("SELECT * FROM api_court WHERE queue_id = %s", [queue.id])
+    if len(list(queue_courts)) == 0:
+        return http_response({}, message='No courts available for this queue', code=400)
+    found_available_court = False;
+    for court in queue_courts:
+        if court.match is None:
+            # Found an empty court
+            found_available_court = True
+
+            # Get the members from the party
+            members_query = Member.objects.raw("SELECT * FROM api_member WHERE party_id = %s", [party_id])
+            members = []
+            for member in members_query:
+                member.party = None
+                member.save()
+                members.append(member)
+
+            # Remove party from queue
+            response = run_connection("DELETE FROM api_party WHERE id = %s", party_id)
+            if response.status_code != 200:
+                # Error
+                return response
+
+            # Create match on court
+            all_matches = Match.objects.raw("SELECT * FROM api_match")
+            largest_id = max([match.id for match in all_matches]) if len(list(all_matches)) > 0 else -1
+            id_of_new_match = largest_id + 1
+
+            now = datetime.datetime.now()
+
+            response = run_connection(
+                "INSERT INTO api_match(id, startDateTime, scoreA, scoreB) VALUES (%s, %s, 0, 0)",
+                id_of_new_match, serializeDateTime(now))
+            if response.status_code != 200:
+                # Error
+                return response
+
+            # Assign teams
+            num_members = len(members)
+            for i in range(num_members):
+                team = "A" if i % 2 == 0 else "B"
+                member = members[i]
+                response = run_connection("INSERT INTO api_playedin(team, match_id, member_id) VALUES (%s, %s, %s)",
+                                          team, id_of_new_match, member.id)
+                if response.status_code != 200:
+                    # Error
+                    return response
+
+            # Add the match to the court
+            response = run_connection("UPDATE api_court SET match_id = %s WHERE id = %s", id_of_new_match, court.id)
+
+            break
+    if found_available_court:
+        return response
+    else:
+        return http_response(message="No available courts", code=400)
+
+
+def refresh_all_queues():
+    """
+    Returns a string of all queues refreshed
+    :return:
+    """
+    queues = Queue.objects.raw("SELECT * FROM api_queue")
+    queues_string = ''
+    for queue in queues:
+        queues_string += '{} '.format(queue.type)
+        response = dequeue_party_to_court_call(queue.type)
+        while response.status_code == 200:
+            response = dequeue_party_to_court_call(queue.type)
+    return queues_string
